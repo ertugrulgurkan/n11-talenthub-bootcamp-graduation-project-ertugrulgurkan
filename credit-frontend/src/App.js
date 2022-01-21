8ce89ca55845fc2d59a97b8262c982986d06a842
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2022-01-21 15:22:59 +0300

    navbar and homepage created some dependencies added

--- credit-frontend/src/App.js
+++ credit-frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './components/home/HomePage';
+import ErrorPage from './components/error/ErrorPage';
+import Menu from './components/menu/Menu';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Menu></Menu>
+        <Routes>
+          <Route path="/" element={<HomePage></HomePage>}></Route>
+          <Route path="*" element={<ErrorPage></ErrorPage>}></Route>
+        </Routes>
+      </div>
+    );
+  }
+
 }
 
 export default App;
