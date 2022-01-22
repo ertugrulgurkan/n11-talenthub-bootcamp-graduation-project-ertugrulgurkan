7f0b060a67f5ffbfea76c35ddc37f5cf48b1166e
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2022-01-23 01:57:36 +0300

    Show Credit Application

--- credit-frontend/src/api/credit/CreditApplicationService.js
+++ credit-frontend/src/api/credit/CreditApplicationService.js
@@ -1,14 +1,22 @@
-
 import axios from "axios";
 
-class CreditApplicationService{
+class CreditApplicationService {
+  saveCreditApplication(newCreditApplication) {
+    var url = "api/v1/credit-applications";
 
-    saveCreditApplication(newCreditApplication){
-        var url = "api/v1/credit-applications";
+    return axios.post(url, newCreditApplication);
+  }
 
-        return axios.post(url, newCreditApplication);
-    }
+  showCreditApplication(nationalIdNumber, birthDate) {
+    var url = "api/v1/credit-applications/";
 
+    return axios.get(url, {
+      params: {
+        birthdate: birthDate,
+        id: nationalIdNumber,
+      },
+    });
+  }
 }
 
 export default new CreditApplicationService();
